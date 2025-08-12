ab37af94376b2066af98eee35e5cb40de1816b0f
Nantha Kishore S <nanthakishore06@gmail.com>
2025-08-12 19:40:28 +0530

    Final Commit

---
+++ RPS-Game/main.js
@@ -0,0 +1,78 @@
+let score= JSON.parse(localStorage.getItem('score')) || {
+    wins:0,
+    losses :0,
+    ties:0
+}
+
+updatescore();
+//Here we are using default operator and reassigning the scores to zero !!!
+//Here we can also use !Not operator instead of checking score === null...
+
+function pcm(){
+    const rn = Math.random();
+    let cm = '';
+    if(rn>=0 && rn<=1/3){
+        cm = 'Rock';
+    }else if(rn>1/3 && rn<=2/3){
+        cm = 'Paper';
+    }else{
+        cm = 'Scissors';
+    } 
+    return cm;
+}
+function playGame(playermove){
+    let cm = pcm();
+    let result = '';
+    if(playermove === 'Rock'){
+        if(cm === 'Rock'){
+            result = 'Tie';
+        }
+        else if(cm === 'Paper'){
+            result = 'You Lose';
+        }
+        else{
+            result = 'You Win !!';
+        }
+    } else if (playermove === 'Paper'){
+            if(cm === 'Rock'){
+                result = 'You Win !!';
+            }
+            else if(cm === 'Paper'){
+                result = 'Tie';
+            }
+            else{
+                result = 'You Lose';
+            }
+    }else if(playermove === 'Scissors'){
+        if(cm === 'Rock'){
+        result = 'You Lose';
+        }
+        else if(cm === 'Paper'){
+            result = 'You Win !!';
+        }
+        else{
+            result = 'Tie';
+        }
+    } 
+
+    if(result === 'You Win !!'){
+        score.wins = score.wins+1;
+    } else if(result === 'You Lose'){
+        score.losses = score.losses+1;
+    } else if(result === 'Tie'){
+        score.ties = score.ties+1;
+    } 
+
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updatescore();      
+    
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = `You <img class="move-icon" src="RPS-images/${playermove}.png" > <img class="move-icon" src="RPS-images/${cm}.png" > Computer`
+        
+}
+
+function updatescore(){
+    document.querySelector('.js-score').innerHTML =  `Wins:${score.wins}  Losses:${score.losses}   Ties:${score.ties}`;
+}
